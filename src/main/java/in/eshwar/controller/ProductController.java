228bdb0bbdc83ec93adefeab928ca89c1603486e
EshwarNaz <eshwarnaz786@gmail.com>
2023-05-09 09:05:48 +0530

    edit functionality added

--- src/main/java/in/eshwar/controller/ProductController.java
+++ src/main/java/in/eshwar/controller/ProductController.java
@@ -1,7 +1,12 @@
 package in.eshwar.controller;
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +20,17 @@ public class ProductController {
 
 	@Autowired
 	private ProductRepository repo;
+
+	@GetMapping("/edit")
+	public String editProduct(@RequestParam("Id") Integer id, Model model) {
+		Optional<ProductEntity> product = repo.findById(id);
+		if (product.isPresent()) {
+			ProductEntity productEntity = product.get();
+			model.addAttribute("product", productEntity);
+		}
+
+		return "index";
+	}
 
 	@GetMapping("/delete")
 	public String deleteProduct(@RequestParam("Id") Integer id, Model model) {
@@ -31,8 +47,12 @@ public class ProductController {
 	}
 
 	@PostMapping("/product")
-	public String saveProduct(@ModelAttribute("product") ProductEntity p, Model model) {
-		repo.save(p);
+	public String saveProduct(@Validated @ModelAttribute("product") ProductEntity p, BindingResult result,
+			Model model) {
+		if (result.hasErrors()) {
+			return "index";
+		}
+		p = repo.save(p);
 		if (p.getId() != null) {
 			model.addAttribute("msg", "product Saved");
 		}
--- src/main/java/in/eshwar/entity/ProductEntity.java
+++ src/main/java/in/eshwar/entity/ProductEntity.java
@@ -3,6 +3,10 @@ package in.eshwar.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -13,7 +17,16 @@ public class ProductEntity {
 	@Id
 	@GeneratedValue
 	private Integer Id;
+	
+	@NotBlank(message="Name is Mandatory")
+	@Size(min=3 , max=15 , message="Name should have only 3 to 15 characters")
 	private String Name;
+	
+	@NotNull(message="Price is Mandatory")
+	@Positive(message="price should be positive")
 	private Double Price;
+	
+	@NotNull(message="Quantity is Mandatory")
+	@Positive(message="Quantity should be positive")
 	private Integer Quantity;
 }
